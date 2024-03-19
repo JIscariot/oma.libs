3b3ebfeaa2b6fc745c32493d1c5191e51ca64455
JIscariot <JIscariot@yandex.com>
2024-03-19 21:04:09 +0300

    WIP

--- src/Unisender/ClientException.php
+++ src/Unisender/ClientException.php
@@ -12,11 +12,12 @@ final class ClientException extends Exception
     public function __construct(string $message = "", string|int $code = 0, ?Throwable $previous = null)
     {
         if (is_string($code)) {
+            var_dump($code);
+
             $code = match ($code) {
                 'unspecified' => 10404,
                 default => 0,
             };
-            var_dump($code);
         }
 
         parent::__construct($message, $code, $previous);
